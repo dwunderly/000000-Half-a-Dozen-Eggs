b9f2dbb34195faec0ae641b4bbb338197960f75f
HenrySmithUCI <hgsmith@uci.edu>
2019-05-20 13:19:24 -0700

    Fix Evo

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -15,14 +15,45 @@ class NN:
         copy.mat = np.copy(self.mat)
         return copy
 
+class Generation:
+    def __init__(self, size, inputs, outputs):
+        self.size = size
+        self.generation = []
+        self.generationT = [None] * size
+        self.best = None
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def beginGeneration():
+        if(self.best == None):
+            for i in range(self.size):
+                self.generation.append(NN(self.inputs, self.outputs))
+        else:
+            for i in range(self.size):
+                c = best.dup()
+                c.mat += np.random.rand(c.inV, c.outV)
+                generation[i] = c
+            
+    def step(self, state, index):
+        f = self.generation[index].forward(np.array(state))
+        i = np.argmax(f)
+        return i
+    
+    def update(self, reward, index):
+        self.generationT[index] = (reward, index)
+
+    def endGeneration():
+        self.generationT.sort()
+        self.best = self.generation[self.generationT[-1][1]].dup()
+        
+
 generation = []
 generationT = []
 best = None
 
 def initialize():
     global best
-    for i in range(100):
-        generation.append(NN(25,8))
+    
 
     for j in range(100):
         r = None
